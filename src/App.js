f48b4651223d01f0e7bfab8065222d5d3a714725
Huynh Dac An <andachuynh@gmail.com>
2023-11-28 19:26:04 +0700

    build global css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Pages/Home";
 import RootLayout from "./Layout/RootLayout";
 import Project from "./Pages/Project";
+import ErrorPage from "./Pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Home /> },
       { path: "project/:projectId", element: <Project /> },
---
+++ src/Pages/Error.js
@@ -0,0 +1,5 @@
+export default function ErrorPage(){
+  return <div className="grid-contain">
+    <h1>Something went wrong</h1>
+  </div>
+}
