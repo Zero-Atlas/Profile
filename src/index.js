2a1c21621f9c1ea43b41048523295be527780561
Huỳnh Đắc An <118910962+Zero-Atlas@users.noreply.github.com>
2023-11-28 18:23:47 +0700

    Re-structure project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Styles/global.css";
+import "./Styles/large-screen.css";
+import "./Styles/medium-screen.css";
+import "./Styles/small-screen.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
