527a879ba69264a17262e8b5adaacf4d1df0ed31
Huynh Dac An <andachuynh@gmail.com>
2023-12-09 17:26:25 +0700

    C2-update Info and Background

--- src/Pages/Content/Background.jsx
+++ src/Pages/Content/Background.jsx
@@ -1,3 +1,133 @@
-export default function Background(){
-  return<div className=""></div>
+import classes from "./Background.module.css";
+
+export default function Background() {
+  return (
+    <div className={classes.wp+' grid-dynamic'}>
+      {/* experience */}
+      <article className={classes.exp}>
+        <div className={classes.title}>
+          <h2>Working experience</h2>
+          <i>icon</i>
+        </div>
+        <ul>
+          <li className={classes["exp-item"]}>
+            <h3>Powersteam VN</h3>
+            <p className={classes.time}>08/2020 - 02/2021</p>
+            <p>Technician:</p>
+            <ul>
+              <li>Training for new technicians</li>
+              <li>Maintenance and clear up cars</li>
+            </ul>
+          </li>
+          <li className={classes["exp-item"]}>
+            <h3>Cartek VN</h3>
+            <p className={classes.time}>05/2019 - 11/2019</p>
+            <p>Collaborators:</p>
+            <ul>
+              <li>Create static web using Dreamweaver</li>
+              <li>Work in team with dropbox</li>
+            </ul>
+          </li>
+        </ul>
+      </article>
+      {/* skill */}
+      <article className={classes.skill}>
+        <div className={classes.title}>
+          <h2>Skills and Techniques</h2>
+          <i>icon</i>
+        </div>
+        <ul>
+          <li className={classes["skill-item"]}>
+            <h3>Programming</h3>
+            <div className={classes.scale}>
+              <p>HTML/CSS : Knowledge</p>
+              <div className={classes.level}>
+                <span className={classes.acquired}></span>
+                <span className={classes.acquired}></span>
+                <span className={undefined}></span>
+                <span className={undefined}></span>
+              </div>
+            </div>
+            <div className={classes.scale}>
+              <p>JavaScript : Knowledge</p>
+              <div className={classes.level}>
+                <span className={classes.acquired}></span>
+                <span className={classes.acquired}></span>
+                <span className={undefined}></span>
+                <span className={undefined}></span>
+              </div>
+            </div>
+            <div className={classes.scale}>
+              <p>ReactJS : Basic</p>
+              <div className={classes.level}>
+                <span className={classes.acquired}></span>
+                <span className={undefined}></span>
+                <span className={undefined}></span>
+                <span className={undefined}></span>
+              </div>
+            </div>
+            <div className={classes.scale}>
+              <p>NodeJs : Basic</p>
+              <div className={classes.level}>
+                <span className={classes.acquired}></span>
+                <span className={undefined}></span>
+                <span className={undefined}></span>
+                <span className={undefined}></span>
+              </div>
+            </div>
+            <div className={classes.scale}>
+              <p>RESTful API : Basic</p>
+              <div className={classes.level}>
+                <span className={classes.acquired}></span>
+                <span className={undefined}></span>
+                <span className={undefined}></span>
+                <span className={undefined}></span>
+              </div>
+            </div>
+
+            <p>Basic skill in MySQL(with Sequelize), MongoDB(with Mongoose)</p>
+          </li>
+          <li className={classes["skill-item"]}>
+            <h3>Language</h3>
+            <p>- English (Good reading and listening skill)</p>
+          </li>
+        </ul>
+      </article>
+      {/*  */}
+      <article className={classes.skill}>
+        <div className={classes.title}>
+          <h2>Education</h2>
+          <i>icon</i>
+        </div>
+        <ul>
+          <li className={classes["edu-item"]}>
+            <h3>BACK-END WEB DEVELOPMENT WITH NODEJS</h3>
+            <p className={classes.time}>5/2023-7/2023</p>
+            <p>Part of CCDN program of FUNiX.</p>
+          </li>
+          <li className={classes["edu-item"]}>
+            <h3>FRONT-END WEB DEVELOPMENT WITH REACT</h3>
+            <p className={classes.time}>3/2023-4/2023</p>
+            <p>Part of CCDN program of FUNiX.</p>
+          </li>
+          <li className={classes["edu-item"]}>
+            <h3>PROGRAMMING TECHNIQUES WITH JAVASCRIPT</h3>
+            <p className={classes.time}>1/2023-2/2023</p>
+            <p>Part of CCDN program of FUNiX.</p>
+          </li>
+          <li className={classes["edu-item"]}>
+            <h3>BUILDING YOUR FIRST WEBSITE</h3>
+            <p className={classes.time}>12/2022</p>
+            <p>Part of CCDN program of FUNiX.</p>
+          </li>
+          <li className={classes["edu-item"]}>
+            <h3>HCMUTE - Ho Chi Minh University of Technology and Education </h3>
+            <p className={classes.time}>08/2015 - 10/2022</p>
+            <p>Major - Automobile Engineer</p>
+            <p>GPA: 7.1/10</p>
+          </li>
+        </ul>
+      </article>
+    </div>
+  );
 }
--- src/Pages/Content/Info.jsx
+++ src/Pages/Content/Info.jsx
@@ -1,12 +1,61 @@
-import classes from './Info.module.css'
+import classes from "./Info.module.css";
 
-export default function Info(){
-  return<div className={classes.wp}>
-    <article className={classes.overview}>
-      <h1>Overview</h1>
-    </article>
-    <article className={classes.info}>
-      <h1>Personal Info</h1>
-    </article>
-  </div>
+export default function Info() {
+  return (
+    <div className={classes.wp}>
+      <article className={classes.overview}>
+        <h2>Overview</h2>
+        <ul>
+          <li>
+            Newbie in programming with good basic and quick learning skills
+          </li>
+          <li>
+            Strengths: Front-end technology and Back-end web application
+            development
+          </li>
+          <li>
+            Knowledge in HTML, CSS, JavaScript - Experience with popular
+            React.js workflows (such as Redux)
+          </li>
+          <li>Knowing RESTful APIs</li>
+          <li>
+            Knowledge in: JavaScript (ReactJS, NodeJs), MySQL (Sequelize),
+            MongoDB (Mongoosse), JSON, API
+          </li>
+          <li>Proficiency in operating systems: Windows</li>
+          <li>Ability to learn and apply new technology quickly</li>
+          <li>Current working location: HCM, Viet Nam</li>
+        </ul>
+      </article>
+      <article className={classes.info}>
+        <h2>Personal Info</h2>
+        <ul className={classes.text}>
+          <li>
+            <span>Name:</span> Huỳnh Đắc An
+          </li>
+          <li>
+            <span>Phone:</span> 0977065543
+          </li>
+          <li>
+            <span>Birth:</span> 13/07/97
+          </li>
+          <li>
+            <span>Email:</span> andachuynh@gmail.com
+          </li>
+          <li>
+            <span>Github:</span>{" "}
+            <a href="https://github.com/Zero-Atlas" target="_blank" rel="noopener noreferrer">
+              https://github.com/Zero-Atlas
+            </a>
+          </li>
+          <li>
+            <span>Leetcode:</span>{" "}
+            <a href="https://leetcode.com/Zero-Atlas/" target="_blank" rel="noopener noreferrer">
+              https://leetcode.com/Zero-Atlas
+            </a>
+          </li>
+        </ul>
+      </article>
+    </div>
+  );
 }
