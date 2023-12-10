47450e719445a3d84db415bc78dab9ee43935fcb
Huynh Dac An <andachuynh@gmail.com>
2023-12-10 16:37:23 +0700

    C3- update certificate

--- src/Pages/Content/Background.jsx
+++ src/Pages/Content/Background.jsx
@@ -2,31 +2,41 @@ import classes from "./Background.module.css";
 
 export default function Background() {
   return (
-    <div className={classes.wp+' grid-dynamic'}>
-      {/* experience */}
-      <article className={classes.exp}>
+    <div className={classes.wp + " grid-dynamic"}>
+      {/* education */}
+      <article className={classes.skill}>
         <div className={classes.title}>
-          <h2>Working experience</h2>
+          <h2>Education</h2>
           <i>icon</i>
         </div>
         <ul>
-          <li className={classes["exp-item"]}>
-            <h3>Powersteam VN</h3>
-            <p className={classes.time}>08/2020 - 02/2021</p>
-            <p>Technician:</p>
-            <ul>
-              <li>Training for new technicians</li>
-              <li>Maintenance and clear up cars</li>
-            </ul>
+          <li className={classes["edu-item"]}>
+            <h3>BACK-END WEB DEVELOPMENT WITH NODEJS</h3>
+            <p className={classes.time}>5/2023-7/2023</p>
+            <p>Part of CCDN program of FUNiX.</p>
           </li>
-          <li className={classes["exp-item"]}>
-            <h3>Cartek VN</h3>
-            <p className={classes.time}>05/2019 - 11/2019</p>
-            <p>Collaborators:</p>
-            <ul>
-              <li>Create static web using Dreamweaver</li>
-              <li>Work in team with dropbox</li>
-            </ul>
+          <li className={classes["edu-item"]}>
+            <h3>FRONT-END WEB DEVELOPMENT WITH REACT</h3>
+            <p className={classes.time}>3/2023-4/2023</p>
+            <p>Part of CCDN program of FUNiX.</p>
+          </li>
+          <li className={classes["edu-item"]}>
+            <h3>PROGRAMMING TECHNIQUES WITH JAVASCRIPT</h3>
+            <p className={classes.time}>1/2023-2/2023</p>
+            <p>Part of CCDN program of FUNiX.</p>
+          </li>
+          <li className={classes["edu-item"]}>
+            <h3>BUILDING YOUR FIRST WEBSITE</h3>
+            <p className={classes.time}>12/2022</p>
+            <p>Part of CCDN program of FUNiX.</p>
+          </li>
+          <li className={classes["edu-item"]}>
+            <h3>
+              HCMUTE - Ho Chi Minh University of Technology and Education{" "}
+            </h3>
+            <p className={classes.time}>08/2015 - 10/2022</p>
+            <p>Major - Automobile Engineer</p>
+            <p>GPA: 7.1/10</p>
           </li>
         </ul>
       </article>
@@ -93,38 +103,30 @@ export default function Background() {
           </li>
         </ul>
       </article>
-      {/*  */}
-      <article className={classes.skill}>
+      {/* experience */}
+      <article className={classes.exp}>
         <div className={classes.title}>
-          <h2>Education</h2>
+          <h2>Working experience</h2>
           <i>icon</i>
         </div>
         <ul>
-          <li className={classes["edu-item"]}>
-            <h3>BACK-END WEB DEVELOPMENT WITH NODEJS</h3>
-            <p className={classes.time}>5/2023-7/2023</p>
-            <p>Part of CCDN program of FUNiX.</p>
+          <li className={classes["exp-item"]}>
+            <h3>Powersteam VN</h3>
+            <p className={classes.time}>08/2020 - 02/2021</p>
+            <p>Technician:</p>
+            <ul>
+              <li>Training for new technicians</li>
+              <li>Maintenance and clear up cars</li>
+            </ul>
           </li>
-          <li className={classes["edu-item"]}>
-            <h3>FRONT-END WEB DEVELOPMENT WITH REACT</h3>
-            <p className={classes.time}>3/2023-4/2023</p>
-            <p>Part of CCDN program of FUNiX.</p>
-          </li>
-          <li className={classes["edu-item"]}>
-            <h3>PROGRAMMING TECHNIQUES WITH JAVASCRIPT</h3>
-            <p className={classes.time}>1/2023-2/2023</p>
-            <p>Part of CCDN program of FUNiX.</p>
-          </li>
-          <li className={classes["edu-item"]}>
-            <h3>BUILDING YOUR FIRST WEBSITE</h3>
-            <p className={classes.time}>12/2022</p>
-            <p>Part of CCDN program of FUNiX.</p>
-          </li>
-          <li className={classes["edu-item"]}>
-            <h3>HCMUTE - Ho Chi Minh University of Technology and Education </h3>
-            <p className={classes.time}>08/2015 - 10/2022</p>
-            <p>Major - Automobile Engineer</p>
-            <p>GPA: 7.1/10</p>
+          <li className={classes["exp-item"]}>
+            <h3>Cartek VN</h3>
+            <p className={classes.time}>05/2019 - 11/2019</p>
+            <p>Collaborators:</p>
+            <ul>
+              <li>Create static web using Dreamweaver</li>
+              <li>Work in team with dropbox</li>
+            </ul>
           </li>
         </ul>
       </article>
--- src/Pages/Content/Certificate.jsx
+++ src/Pages/Content/Certificate.jsx
@@ -1,3 +1,39 @@
-export default function Certificate(){
-  return<></>
+import classes from "./Certificate.module.css";
+import cert_1 from "../../Assets/Cert_WEB.PNG";
+import cert_2 from "../../Assets/Cert_PRF.PNG";
+import cert_3 from "../../Assets/Cert_RJS.PNG";
+import cert_4 from "../../Assets/Cert_NJS.PNG";
+
+export default function Certificate() {
+  const images = [cert_1, cert_2, cert_3, cert_4];
+  const names = [
+    "BUILDING YOUR FIRST WEBSITE",
+    "PROGRAMMING TECHNIQUES WITH JAVASCRIPT",
+    "FRONT-END WEB DEVELOPMENT WITH REACT",
+    "BACK-END WEB DEVELOPMENT WITH NODEJS",
+  ];
+  const contents = [
+    "Part of CCDN program of FUNIX.",
+    "Part of CCDN program of FUNIX.",
+    "Part of CCDN program of FUNIX.",
+    "Part of CCDN program of FUNIX.",
+  ];
+  return (
+    <div className={classes.wp + " grid-contain"}>
+      <h2>Certificates and rewards</h2>
+      <ul className="d-grid grid-2-col">
+        {images.map((item, i) => (
+          <li key={i} className={classes["cert-item"]}>
+            <div className={classes.img}>
+              <img src={item} alt={names[i] + " Huỳnh Đắc An"} />
+            </div>
+            <div className={classes.text}>
+              <h3>{names[i]}</h3>
+              <p>{contents[i]}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
