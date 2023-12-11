e52b8d56044a32dac1973e16e2b7532c5648b565
Huynh Dac An <andachuynh@gmail.com>
2023-12-11 22:58:53 +0700

    C4-adding project

--- src/Pages/Content/Projects.jsx
+++ src/Pages/Content/Projects.jsx
@@ -1,3 +1,24 @@
+import classes from './Projects.module.css'
+import cv from '../../Assets/Project_CV.PNG'
+import booking from '../../Assets/Project_Booking.PNG'
+import novel from '../../Assets/Project_Novel.PNG'
+import { Link } from 'react-router-dom'
+
 export default function Projects(){
-  return<></>
+  const images=[cv,booking,novel]
+  const name=['cv','booking','novel']
+
+  return<div className={classes.wp}>
+  <h2>Project</h2>
+  <ul className='d-grid grid-3-col'>
+    {images.map((item,i)=>
+    <li key={i} className={classes.project}>
+      <Link to={'/project/'+name[i]} >
+      <div className={classes.img}><img src={item} alt={name[i]+' Huỳnh Đắc An'} /></div>
+      </Link>
+
+    </li>
+      )}
+  </ul>
+</div>
 }
